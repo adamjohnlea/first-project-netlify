e64e3fb31ef530d43dbad8d9f7bfd6b4450c0a82
adamjohnlea <imnogeekdptcom@me.com>
2020-11-18 16:17:51 -0700

    update for deployment

--- pages/pokemon/[pokemon].js
+++ pages/pokemon/[pokemon].js
@@ -15,7 +15,7 @@ function Pokemon({ pokemon }) {
     )       
 }
 
-export async function getStaticPaths() {
+export async function getStaticPaths({locale}) {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
     const pokemon = await res.json()
 
